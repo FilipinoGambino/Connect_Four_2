811d911f75a95d95fff91a80acc80287c48e9a3d
Nicholas Gorichs <ngorichs1@gmail.com>
2024-01-04 16:34:09 -0600

    understanding einsum

--- scratches/attention.py
+++ scratches/attention.py
@@ -12,16 +12,22 @@ class AttnVector(nn.Module):
             self,
             in_channels,
             heads,
+            bias
     ):
         super(AttnVector, self).__init__()
         self.heads = heads
+        self.qkv_dim = in_channels // heads
 
-        self.w = nn.Linear(in_channels, in_channels)
+        self.w = nn.Linear(in_channels, in_channels, bias=bias)
 
         # nn.init.normal_(self.w, std=0.01)
 
     def forward(self, x: torch.Tensor):
+        print(f'---  x:{x.shape}  ---')
+        shape = x.shape[:-1]
         out = self.w(x)
+        out = out.view(*shape, self.heads, self.qkv_dim)
+        print(f'---  out:{out.shape}  ---')
         return out
 
 
@@ -39,31 +45,33 @@ class AttnBlock(nn.Module):
         self.qkv_dim = dim // heads
 
         print(f"dim: {dim} | heads: {heads} | qkv_dim: {dim // heads}")
-        self.q = AttnVector(dim, heads)
-        self.k = AttnVector(dim, heads)
-        self.v = AttnVector(dim, heads)
+        self.q = AttnVector(dim, heads, bias)
+        self.k = AttnVector(dim, heads, bias)
+        self.v = AttnVector(dim, heads, bias)
 
         self.scale = self.qkv_dim ** -0.5
 
         self.drop = nn.Dropout(0.1)
 
     def forward(self, x, mask):
-        # print(f"embds shape: {x.shape}")
         query = self.q(x)
-        key = self.k(x)
+        key = self.k(x).mT
         value = self.v(x)
         print(f"query shape: {query.shape}\nkey shape: {key.shape}\nvalue shape: {value.shape}\n")
-        weights = torch.einsum('bhwc,bpqd->bhwpq', query, key)
-        # print(f"weights shape: {weights.shape}")
+        weights = torch.einsum('bhwnd, bhwdn -> bnhw', query, key)
+        print(f"weights shape: {weights.shape}")
+        print(query)
+        print(key)
+        print(weights)
         if mask is not None:
             raise NotImplementedError
 
         weights = self.drop(weights)
         weights *= self.scale
         weights = F.softmax(weights, dim=-1)
-        # print(f"value shape: {value.shape}")
-        attn = weights @ value
 
+        attn = torch.einsum('bnhwd, bnxyd -> bnhwxy', weights, value)
+        #
         return attn
 '''
 bs = batch size
@@ -72,8 +80,21 @@ w = width
 c = channels
 e = embeddings
 '''
-matrix = torch.randint(low=0, high=2, size=[8,6,7,4], dtype=torch.float32)
-# emb = nn.Embedding(2,16)
-# embs = emb(matrix)
-attn_block = AttnBlock(matrix.shape[-1], 4)
+matrix = torch.randint(low=0, high=2, size=[1,2,2,4], dtype=torch.float32)
+attn_block = AttnBlock(matrix.shape[-1], 2, False)
 print(attn_block(matrix, None))
+
+# print(f'---  matrix  ---')
+# shapes = matrix.shape[:-1]
+# matrix = matrix.view(*shapes, 3, 2)
+# print(matrix.shape)
+# print(matrix)
+# print(matrix.mT)
+# weights = torch.einsum('bhwnd, bhwnd -> bhwn', matrix, matrix)
+# print(weights)
+# attn = None
+# a = torch.Tensor([[2,4],[3,1],[1,3]])
+# b = torch.Tensor([[2,3,4],[3,1,4]])
+# print(a, a.shape)
+# print(b, b.shape)
+# print(torch.einsum('xy,yz->xz', b, a))
