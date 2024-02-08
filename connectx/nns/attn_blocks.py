6a973e30980959202fd078d84cb377d57767b2bd
FilipinoGambino <ngorichs1@gmail.com>
2024-02-07 20:47:48 -0600

    ViT Block not completing logging.info

--- connectx/nns/attn_blocks.py
+++ connectx/nns/attn_blocks.py
@@ -106,6 +106,7 @@ class ViTBlock(nn.Module):
                 kernel_size=(1, 1)
             )
         )
+        logging.info('Finished making the ViT Block')
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         logging.info(f"Inside ViT Block")
